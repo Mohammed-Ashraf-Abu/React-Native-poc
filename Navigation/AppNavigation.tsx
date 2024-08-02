c13cef99e8f5a1d718951c774f1aae8b90d8269e
Mohammed Ashraf <contact.ashrafabuthyar@gmail.com>
2024-08-02 20:38:16 +0530

    file missed from local

--- Navigation/AppNavigation.tsx
+++ Navigation/AppNavigation.tsx
@@ -1,19 +1,20 @@
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import HomeScreen from '../Screens/homescreen/HomeScreen';
-import LoginScreen from '../Screens/loginscreen/LoginScreen';
-import ProfileScreen from '../Screens/profile/ProfileScreen';
-import SettingScreen from '../Screens/setting/SettingScreen';
-import DrawerNavigation from './DrawerNavigation';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "../Screens/homescreen/HomeScreen";
+import LoginScreen from "../Screens/loginscreen/LoginScreen";
+import ProfileScreen from "../Screens/profile/ProfileScreen";
+import SettingScreen from "../Screens/setting/SettingScreen";
+import DrawerNavigation from "./DrawerNavigation";
 
-
+const Stack = createNativeStackNavigator();
 
 export const NavigationMain = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        screenOptions={{headerShown: true}}
-        initialRouteName="Main">
+        screenOptions={{ headerShown: true }}
+        initialRouteName="Main"
+      >
         <Stack.Screen
           name="Main"
           component={DrawerNavigation}
