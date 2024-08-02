a986798b97e62db006d664e4ec53306dd68b4bfb
Mohammed Ashraf <contact.ashrafabuthyar@gmail.com>
2024-08-02 20:14:14 +0530

    Code cleanup

--- Navigation/AppNavigation.tsx
+++ Navigation/AppNavigation.tsx
@@ -1,34 +1,10 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from '../Screens/homescreen/HomeScreen';
-// import {createAppContainer} from 'react-navigation';
 import LoginScreen from '../Screens/loginscreen/LoginScreen';
 import ProfileScreen from '../Screens/profile/ProfileScreen';
 import SettingScreen from '../Screens/setting/SettingScreen';
 import DrawerNavigation from './DrawerNavigation';
-// import {createStackNavigator} from 'react-navigation-stack';
-
-const Stack = createNativeStackNavigator();
-// const Router = createStackNavigator(
-//   {
-//     HomeScreen,
-//     LoginScreen,
-//     ProfileScreen,
-//     SettingScreen,
-//     DrawerNavigation,
-//   },
-//   {
-//     initialRouteName: 'LoginScreen',
-//     headerMode: 'none',
-//   },
-// );
-
-// export type Navigation = {
-//   reset: any;
-//   navigate: (scene: string) => void;
-// };
-
-// export default createAppContainer(Router);
 
 
 
--- Navigation/DrawerNavigation.tsx
+++ Navigation/DrawerNavigation.tsx
@@ -62,17 +62,7 @@ export default function DrawerNavigation() {
           headerShadowVisible: false,
         }}
       />
-      {/* <Drawer.Screen
-        name=" Home"
-        component={HomeScreen}
-        options={{
-          // headerBackground:{},
-          // drawerStyle:{backgroundColor:"blue"},
-          drawerLabel: 'Home',
-          headerShown: true,
-          headerShadowVisible: false,
-        }}
-      /> */}
+
 
       <Drawer.Screen
         name="Profile"
@@ -97,73 +87,4 @@ export default function DrawerNavigation() {
   );
 }
 
-// import {
-//   createDrawerNavigator,
-//   DrawerContentComponentProps,
-//   DrawerItemList,
-// } from '@react-navigation/drawer';
-// import React from 'react';
-// import {SafeAreaView, View, Text} from 'react-native';
-// import { BottomTabNavigation } from './TabNavigation';
-// import LoginScreen from '../Screens/login screen/LoginScreen';
-// import SettingScreen from '../Screens/setting/SettingScreen';
-// import ProfileScreen from '../Screens/profile/ProfileScreen';
-// import AvatarImage from '../Components/image/Avatar/Avatar';
-// import Labelcomponent from '../Components/text/Text';
 
-// const Drawer = createDrawerNavigator();
-
-// const CustomDrawerContent: React.FC<DrawerContentComponentProps> = props => (
-//   <SafeAreaView style={{flex: 1}}>
-//     <View
-//       style={{
-//         marginTop: 20,
-//         marginBottom: 20,
-//         borderColor: 'rgb(136 35 108)',
-//         borderWidth: 4,
-//         borderRadius: 100,
-//         justifyContent: 'center',
-//         alignSelf: 'center',
-//       }}>
-//       <AvatarImage />
-//     </View>
-//     <View style={{marginBottom: 10}}>
-//       <Labelcomponent value={'Hello Admin'} type={'Heading'} />
-//     </View>
-//     <DrawerItemList {...props} />
-//   </SafeAreaView>
-// );
-
-// export default function DrawerNavigation() {
-//   return (
-//     <Drawer.Navigator
-//       drawerContent={props => <CustomDrawerContent {...props} />}
-//       screenOptions={{
-//         drawerStyle: {
-//           backgroundColor: 'white',
-//           width: 250,
-//         },
-//       }}>
-//       <Drawer.Screen
-//         name="Tabs"
-//         component={BottomTabNavigation}
-//         options={{drawerLabel: 'Home'}}
-//       />
-//       <Drawer.Screen
-//         name="Profile"
-//         component={ProfileScreen}
-//         options={{drawerLabel: 'Profile'}}
-//       />
-//       <Drawer.Screen
-//         name="Settings"
-//         component={SettingScreen}
-//         options={{drawerLabel: 'Settings'}}
-//       />
-//       <Drawer.Screen
-//         name="Login"
-//         component={LoginScreen}
-//         options={{drawerLabel: 'Logout'}}
-//       />
-//     </Drawer.Navigator>
-//   );
-// }
